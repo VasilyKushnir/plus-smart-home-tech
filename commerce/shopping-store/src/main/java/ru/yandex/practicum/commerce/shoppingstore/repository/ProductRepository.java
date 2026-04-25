8cdf69cfbe306be48c2a62fe3010f9faf1ab725c
Wassili Kuschnir <wassili.kuschnir@ya.ru>
2026-04-25 07:40:11 +0700

    chore: remove redundant @Repository annotation

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/commerce/shoppingstore/repository/ProductRepository.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/commerce/shoppingstore/repository/ProductRepository.java
@@ -3,13 +3,11 @@ package ru.yandex.practicum.commerce.shoppingstore.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.commerce.interactionapi.dto.ProductCategory;
 import ru.yandex.practicum.commerce.shoppingstore.entity.Product;
 
 import java.util.UUID;
 
-@Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     Page<Product> findByProductCategory(ProductCategory category, Pageable pageable);
 }
